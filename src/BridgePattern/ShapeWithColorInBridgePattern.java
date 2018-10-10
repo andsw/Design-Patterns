980c3f8e8bf40bb4c1abe6cfd695556bbac6144b
何少文 <985934131@qq.com>
2018-10-10 20:14:02 +0800

    Add two class in bridge pattern, and also add two corresponding UML images.

---
+++ src/BridgePattern/ShapeWithColorInBridgePattern.java
@@ -0,0 +1,73 @@
+package BridgePattern;
+
+/**
+ * @author hsw
+ * @create 2018-10-10  20:00
+ */
+
+interface Color {
+    void bePainted(String shapeName);
+}
+
+abstract class Shape {
+    Color color;
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public abstract void draw();
+}
+
+class Circle extends Shape{
+
+    public void draw() {
+        color.bePainted("正方形");
+    }
+}
+
+class Rectangle extends Shape{
+
+    public void draw() {
+        color.bePainted("长方形");
+    }
+
+}
+
+class Square extends Shape{
+
+    public void draw() {
+        color.bePainted("正方形");
+    }
+
+}
+
+class White implements Color{
+
+    public void bePainted(String shape) {
+        System.out.println("白色的" + shape);
+    }
+}
+
+class Gray implements Color{
+
+    public void bePainted(String shape) {
+        System.out.println("灰色的" + shape);
+    }
+}
+
+class Black implements Color{
+
+    public void bePainted(String shape) {
+        System.out.println("黑色的" + shape);
+    }
+}
+
+public class ShapeWithColorInBridgePattern {
+    public static void main(String[] args) {
+        Color white = new White();
+        Shape square = new Square();
+        square.setColor(white);
+        square.draw();
+    }
+}
---
+++ src/BridgePattern/VideoPlayerInBridgePattern.java
@@ -0,0 +1,91 @@
+package BridgePattern;
+
+/**
+ * @author hsw
+ * @create 2018-10-10  19:08
+ */
+
+interface VideoFile{
+    void decode(String osType, String fileName);
+}
+
+class MPEGFile implements VideoFile {
+
+    @Override
+    public void decode(String osType, String fileName) {
+        System.out.println("this is a mpeg video named " + fileName + " that played on " + osType + "operation system!");
+    }
+}
+
+class RMVBFile implements VideoFile {
+
+    @Override
+    public void decode(String osType, String fileName) {
+        System.out.println("this is a rmvb video named " + fileName + " that played on " + osType + "operation system!");
+    }
+}
+
+class AVIFile implements VideoFile {
+
+    @Override
+    public void decode(String osType, String fileName) {
+        System.out.println("this is a avi video named " + fileName + " that played on " + osType + "operation system!");
+    }
+}
+
+class WMVFile implements VideoFile {
+
+    @Override
+    public void decode(String osType, String fileName) {
+        System.out.println("this is a wmv video named " + fileName + " that played on " + osType + "operation system!");
+    }
+}
+
+
+abstract class OperationSystemVersion{
+    protected VideoFile videoFile;
+
+    public void setVideoFile(VideoFile videoFile) {
+        this.videoFile = videoFile;
+    }
+
+    abstract public void play(String fileName);
+}
+
+class WindowsVersion extends OperationSystemVersion {
+
+    @Override
+    public void play(String fileName) {
+        videoFile.decode("windows", fileName);
+    }
+}
+
+class LinuxVersion extends OperationSystemVersion {
+
+    @Override
+    public void play(String fileName) {
+        videoFile.decode("linux", fileName);
+
+    }
+}
+
+class MacVersion extends OperationSystemVersion {
+
+    @Override
+    public void play(String fileName) {
+        videoFile.decode("macOS", fileName);
+    }
+}
+
+
+
+public class VideoPlayerInBridgePattern {
+
+    public static void main(String[] args) {
+        MacVersion macVersion = new MacVersion();
+        VideoFile videoFile = new AVIFile();
+        macVersion.setVideoFile(videoFile);
+        macVersion.play("video-10");
+    }
+
+}
